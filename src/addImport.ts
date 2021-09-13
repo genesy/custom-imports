7868a30618421fcd5faceb4d2b29d95a930736a6
Gene Sy <gnsyftw@gmail.com>
2021-09-14 05:37:34 +0800

    Moves addimport to separate file

--- src/addImport.ts
+++ src/addImport.ts
@@ -1,2 +1,68 @@
+import ts from 'typescript';
+import * as vsc from 'vsc-base';
 
-const addImport = ()
+const addImportToFile = (
+  sourceFile: ts.SourceFile,
+  importSource: string,
+  variable: string,
+  isDefault = false,
+) => {
+  // TODO: scan file or tsconfig for actual format requirements
+  const SPACE = ' ';
+  const SEMICOLON = ';';
+  const QUOTE = "'";
+
+  const importStatements: ts.Node[] = [];
+  let theImport: ts.Node | undefined;
+  let firstExpression: ts.Node | undefined;
+
+  sourceFile.statements.forEach((node, index) => {
+    if (!ts.isImportDeclaration(node)) {
+      firstExpression = node;
+      return;
+    }
+    importStatements.push(node);
+
+    // matches the name of the module/source path
+    const re = new RegExp(`(["'])${importSource}\\1`);
+    if (node.getFullText().match(re)) {
+      theImport = node;
+    }
+  });
+
+  const lastImport = importStatements[importStatements.length - 1];
+
+  if (theImport) {
+    const importStatement = theImport.getFullText();
+    const openingBrace = `{${SPACE}`;
+    const openingBraceIndex = importStatement.indexOf(openingBrace);
+    if (!isDefault) {
+      if (openingBraceIndex !== -1) {
+        // insert before lasing closing brace
+        const closingBrace = `${SPACE}}`;
+        const closingBraceIndex = importStatement.indexOf(closingBrace);
+        vsc.insertAt(`, ${variable}`, theImport.pos + closingBraceIndex);
+        // content = vsc.insertAfter(content, /\{/, `${variable},${SPACE}`);
+      } else {
+        const fromTextIndex = importStatement.indexOf(' from ');
+        vsc.insertAt(`,${SPACE}{${SPACE}${variable}${SPACE}}`, theImport.pos + fromTextIndex);
+      }
+    } else {
+      vsc.insertAt(`${variable}, `, theImport.pos + 8); // 8 because `import ` length + 1 is static
+    }
+  } else if (lastImport) {
+    if (!isDefault) {
+      vsc.insertAt(
+        `\nimport {${SPACE}${variable}${SPACE}} from ${QUOTE}${importSource}${QUOTE}${SEMICOLON}\n`,
+        lastImport.end,
+      );
+    } else {
+      vsc.insertAt(
+        `\nimport ${SPACE}${variable}${SPACE} from ${QUOTE}${importSource}${QUOTE}${SEMICOLON}\n`,
+        lastImport.end,
+      );
+    }
+  } else {
+    // TODO: for empty files
+  }
+};
--- src/extension.ts
+++ src/extension.ts
@@ -5,76 +5,19 @@ import { data } from './data.json';
 import { PACKAGE_NAME } from './config';
 import * as ts from 'typescript';
 import * as vsc from 'vsc-base';
+import addImportToFile from './addImport';
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.window.showInformationMessage('CUSTOM IMPORTS ACTIVE');
 
   vscode.commands.registerCommand(
     `${PACKAGE_NAME}.import`,
-    (variable, source: string, isDefault) => {
+    (variable, importSource: string, isDefault) => {
       const document = vsc.getActiveDocument();
       let content = vsc.getDocumentContent();
       if (!content || !document) return;
       const sourceFile = vsc.tsCreateSourceFile(content);
-
-      // TODO: scan file or tsconfig for actual format requirements
-      const SPACE = ' ';
-      const SEMICOLON = ';';
-      const QUOTE = "'";
-
-      const importStatements: ts.Node[] = [];
-      let theImport: ts.Node | undefined;
-      let firstExpression: ts.Node | undefined;
-
-      sourceFile.statements.forEach((node, index) => {
-        if (!ts.isImportDeclaration(node)) {
-          firstExpression = node;
-          return;
-        }
-        importStatements.push(node);
-
-        // matches the name of the module/source path
-        const re = new RegExp(`(["'])${source}\\1`);
-        if (node.getFullText().match(re)) {
-          theImport = node;
-        }
-      });
-
-      const lastImport = importStatements[importStatements.length - 1];
-
-      if (theImport) {
-        const importStatement = theImport.getFullText();
-        const openingBrace = `{${SPACE}`;
-        const openingBraceIndex = importStatement.indexOf(openingBrace);
-        if (!isDefault) {
-          if (openingBraceIndex !== -1) {
-            // insert before lasing closing brace
-            const closingBrace = `${SPACE}}`;
-            const closingBraceIndex = importStatement.indexOf(closingBrace);
-            vsc.insertAt(`, ${variable}`, theImport.pos + closingBraceIndex);
-            // content = vsc.insertAfter(content, /\{/, `${variable},${SPACE}`);
-          } else {
-            const fromTextIndex = importStatement.indexOf(' from ');
-            vsc.insertAt(`,${SPACE}{${SPACE}${variable}${SPACE}}`, theImport.pos + fromTextIndex);
-          }
-        } else {
-          vsc.insertAt(`${variable}, `, theImport.pos + 8); // 8 because `import ` length + 1 is static
-        }
-      } else if (lastImport) {
-        if (!isDefault) {
-          vsc.insertAt(
-            `\nimport {${SPACE}${variable}${SPACE}} from ${QUOTE}${source}${QUOTE}${SEMICOLON}\n`,
-            lastImport.end,
-          );
-        } else {
-          vsc.insertAt(
-            `\nimport ${SPACE}${variable}${SPACE} from ${QUOTE}${source}${QUOTE}${SEMICOLON}\n`,
-            lastImport.end,
-          );
-        }
-      } else {
-        // TODO: for empty files
-      }
+      addImportToFile(sourceFile, importSource, variable, isDefault);
     },
   );
 
@@ -90,16 +33,24 @@ export function activate(context: vscode.ExtensionContext) {
       ) {
         let items: vscode.CompletionItem[] = [];
 
-        data.forEach(({ exports, source }) => {
-          // TODO: don't show if already imported
+        const createItem = (name: string, source: string, isDefault = false) => {
+          const item = new vscode.CompletionItem(name, vscode.CompletionItemKind.Variable);
+          item.detail = `::import from ${source}`;
+          item.command = {
+            title: 'import',
+            command: `${PACKAGE_NAME}.import`,
+            arguments: [name, source, isDefault],
+          };
+          return item;
+        };
+        data.forEach(({ exports, source, defaultName = '' }) => {
+          // TODO: don't show completionitem if already imported
+          if (defaultName) {
+            const item = createItem(defaultName, source, true);
+            items.push(item);
+          }
           exports.forEach((exportName) => {
-            const item = new vscode.CompletionItem(exportName, vscode.CompletionItemKind.Variable);
-            item.detail = `::import from ${source}`;
-            item.command = {
-              title: 'import',
-              command: `${PACKAGE_NAME}.import`,
-              arguments: [exportName, source],
-            };
+            const item = createItem(exportName, source);
             const asIndex = exportName.indexOf(' as ');
             const hasAlias = asIndex !== -1;
             if (hasAlias) {
